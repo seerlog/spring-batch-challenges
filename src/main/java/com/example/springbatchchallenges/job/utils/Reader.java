7b9765b840417947deaea1086777c63b77cabd07
seerlog <selfandtogether@gmail.com>
2024-08-21 02:03:03 +0900

    날짜 파싱 에러 예외처리 추가

--- src/main/java/com/example/springbatchchallenges/job/utils/Reader.java
+++ src/main/java/com/example/springbatchchallenges/job/utils/Reader.java
@@ -9,10 +9,14 @@ import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.transform.FieldSet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.regex.Pattern;
 
 @Configuration
@@ -39,6 +43,9 @@ public class Reader {
                     }
                 }
                 // Check if the value contains invalid date
+                if(!isCorrectDateFormat(fieldSet)) {
+                    throw new RuntimeException("Invalid date format");
+                }
 
                 return RestaurantVO.builder()
                         .restaurantCsvVO(RestaurantCsvVO.of(fieldSet))
@@ -83,5 +90,25 @@ public class Reader {
             return true;
         }
         return false;
+    }
+
+    private boolean isCorrectDateFormat(FieldSet fieldSet) {
+        try {
+            // date
+            LocalDate licensingDate = LocalDate.parse(fieldSet.readString("licensingDate"));
+            LocalDate licensingCancelDate = LocalDate.parse(fieldSet.readString("licensingCancelDate"));
+            LocalDate closingDate = LocalDate.parse(fieldSet.readString("closingDate"));
+            LocalDate suspensionStartDate = LocalDate.parse(fieldSet.readString("suspensionStartDate"));
+            LocalDate suspensionEndDate = LocalDate.parse(fieldSet.readString("suspensionEndDate"));
+            LocalDate reopeningDate = LocalDate.parse(fieldSet.readString("reopeningDate"));
+
+            // datetime
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            LocalDateTime lastModifiedDatetime = LocalDateTime.parse(fieldSet.readString("lastModifiedDatetime"), formatter);
+            LocalDateTime dataRenewalDate = LocalDateTime.parse(fieldSet.readString("dataRenewalDate"), formatter);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
